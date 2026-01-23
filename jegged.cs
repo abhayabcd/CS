2e6431d4a02c70a59dba354be01507f309a5c5a0
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2026-01-23 11:31:51 +0530

    Add files via upload
    23/1/2025

---
+++ jegged.cs
@@ -0,0 +1,32 @@
+using System;
+class Jegged
+{
+    static void Main()
+    {
+        int[][] j = new int[4][];
+
+        j[0] = new int[4];
+        j[1] = new int[1];
+        j[2] = new int[3];
+        j[3] = new int[2];
+
+        // Input
+        for (int i = 0; i < j.Length; i++)
+        {
+            for (int k = 0; k < j[i].Length; k++)
+            {
+                j[i][k] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // Output
+        Console.WriteLine("\nJagged Array Elements:");
+        for (int i = 0; i < j.Length; i++)
+        {
+            for (int k = 0; k < j[i].Length; k++)
+            {
+                Console.WriteLine(j[i][k]);
+            }
+        }
+    }
+}
