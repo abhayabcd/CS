f52f22bd827ce9d814f62177256c198d75da463e
Abhay Gondaliya <abhaygondaliya881@gmail.com>
2026-02-13 10:22:25 +0530

    Add files via upload
    13/2/2026

---
+++ Privet.cs
@@ -0,0 +1,12 @@
+using System;
+class Privet
+{
+    Privet()
+    {
+        Console.WriteLine("This is a constructor.");
+    }
+    static void Main()
+    {
+        Privet obj = new Privet();
+    }
+}
